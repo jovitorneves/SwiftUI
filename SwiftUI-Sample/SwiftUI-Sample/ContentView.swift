a4eb80d207489f65e8c2d5b74ab8d848ddadbd0d
Vitor neves <j.victtor.neves@hotmail.com>
2019-11-15 20:11:14 -0300

    add project

---
+++ SwiftUI-Sample/SwiftUI-Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sample
+//
+//  Created by joao.vitor.f.naves on 15/11/19.
+//  Copyright © 2019 vitor.neves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Sample/SwiftUI-SampleTests/SwiftUI_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_SampleTests.swift
+//  SwiftUI-SampleTests
+//
+//  Created by joao.vitor.f.naves on 15/11/19.
+//  Copyright © 2019 vitor.neves. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_Sample
+
+class SwiftUI_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
